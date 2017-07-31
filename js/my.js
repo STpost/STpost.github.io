ce7080f81172deac8660af1551d2a902f954a552
a-morr <adampmorrow@gmail.com>
2017-07-31 14:26:21 -0600

    'f'

--- js/my.js
+++ js/my.js
@@ -1,7 +1,6 @@
 $(document).ready( function(){
     $('#button').click(function () {
-       var toAdd =  "1000";
-       $('.total').append(toAdd);
+       $(this).toggleClass("highlighted");
 
     });  
 });
