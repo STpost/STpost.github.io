5631355bcb7c8726501b1de0a08c77ccda41c98f
a-morr <adampmorrow@gmail.com>
2017-08-01 07:18:38 -0600

    'f'

--- js/my.js
+++ js/my.js
@@ -1,7 +1,6 @@
 $(document).ready( function(){
     $('#text').click( function() {
     	var len = $('#length').val()
-        $(this).toggleClass("highlighted");
         $('#total').val(len);
     });
 });
