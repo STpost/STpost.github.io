62360d6fdd882e0bf97b30eb854760dd224ae50f
a-morr <adampmorrow@gmail.com>
2017-08-01 07:33:15 -0600

    'f'

--- js/my.js
+++ js/my.js
@@ -1,7 +1,9 @@
 $(document).ready( function(){
     $('#calc').click( function() {
     	var len = $('#length').attr('value')
-    	alert(len);
+    	var wid = $('#width').attr('value')
+    	var cos = $('#cost').attr('value')
+    	len = len*wid/cost
         $('#total').val(len);
     });
 });
